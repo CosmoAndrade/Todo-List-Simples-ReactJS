a87dc6660bcc8f19b38246b425399b4dffe5ee37
CosmoAndrade <cosmo.andrade@hotmail.com>
2022-03-19 16:56:09 -0300

    TodoLis

---
+++ src/App.js
@@ -0,0 +1,57 @@
+
+import './App.css';
+import { useState } from 'react';
+
+
+function App() {
+
+ const [task, setTask] = useState('');
+  const [itemsList, setItemsList] = useState([]);
+
+  function handleChangeInput(event) {
+    const inputTask = event.target.value;
+    
+    setTask(inputTask);
+  }
+
+  function handleAddItemToList(event) {
+    event.preventDefault();
+
+    /**
+     * Evita o usuário adicionar uma tarefa sem nome
+     */
+    if (task) {
+      /**
+       * Adiciona no final no array a nova tarefa
+       */
+      setItemsList([...itemsList, task]);
+      
+      // Limpa o campo de input
+      setTask("");
+    }
+  }
+
+  return (
+    <div className="App" >
+
+      <form onSubmit={handleAddItemToList}>
+        <h1>ToDo</h1>
+
+        <input type="text" onChange={(event) => handleChangeInput(event)} value={task} />
+        <button type="submit">Add</button>
+        
+       
+      </form>
+
+      {itemsList.map((item, index) => (
+        <div key={index} >{item}</div>
+      ))}
+
+
+      </div>
+    )
+    
+  
+}
+
+export default App;
